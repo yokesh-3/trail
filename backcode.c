333cfaa0ddad73303f02535f6c5457c7b39e7fab
yokesh-3 <yokeshb7@gmail.com>
2024-02-21 15:46:22 +0530

    feat:data updated

--- backcode.c
+++ backcode.c
@@ -2,8 +2,8 @@
 void main()
 {
     int a=1,b=2;
-    printf("%d",a+b);
-    printf("%d",a-b);
-    printf("%d",a*b);
-    printf("%d",a/b);
+    printf(" %d ",a+b);
+    printf(" %d ",a-b);
+    printf(" %d ",a*b);
+    printf(" %d ",a/b);
 }
