a2226f5307d31e5c96a49de74266b0b761c6d5da
YOKESH B <160481691+yokesh-3@users.noreply.github.com>
2024-02-21 10:42:37 +0530

    Update backcode.c

--- backcode.c
+++ backcode.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 void main()
 {
-    int a=1234,b=5678;
+    int a=1,b=2;
     printf("%d",a+b);
     printf("%d",a-b);
-    printf("%d",a*b);
-    printf("%d",a/b);
-    hello all
 }
